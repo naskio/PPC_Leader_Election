20ebfde4d0ece4b033295582a48ae6a7f2adb426
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2021-02-28 12:33:45 +0100

    adding TODOs

--- src/main/scala/DisplayActor.scala
+++ src/main/scala/DisplayActor.scala
@@ -2,15 +2,13 @@ package upmc.akka.leader
 
 import akka.actor._
 
-case class Message (content:String)
+case class Message(content: String)
 
 class DisplayActor extends Actor {
 
-     def receive = {
-
-          case Message (content) => {
-               println(content)
-          }
-
-     }
+  def receive = {
+    case Message(content) => {
+      println(content)
+    }
+  }
 }
